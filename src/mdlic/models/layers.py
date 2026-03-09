e3ddd49a339efb3a51593b5a4c2104715b8ec2c5
yourauroral <cm4002004@163.com>
2026-03-09 20:52:46 +0800

    feat/flash-attention

--- src/mdlic/models/layers.py
+++ src/mdlic/models/layers.py
@@ -14,9 +14,10 @@ class LayerNormalization(nn.Module):
         # Keep the dimension for broadcasting
       mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
       # Keep the dimension for broadcasting
-      std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
-      # eps is to prevent dividing by zero or when std is very small
-      return self.alpha * (x - mean) / (std + self.eps) + self.bias
+      var = x.var(dim = -1, keepdim = True, unbiased=False) # (batch, seq_len, 1)
+      # eps is to prevent dividing by zero or when var is very small
+      x = (x - mean) / torch.sqrt(var + self.eps) 
+      return self.alpha * x + self.bias
 
 class FeedForwardBlock(nn.Module):
   def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
@@ -51,6 +52,7 @@ class ResidualBlock(nn.Module):
   def forward(self, x):
     return self.act(x + self.net(x)) 
 
+from .flash_attn import TritonAttention
 class MultiHeadAttentionBlock(nn.Module):
   def __init__(self, d_model: int, h: int, dropout: float) -> None:
       super().__init__()
@@ -66,43 +68,64 @@ class MultiHeadAttentionBlock(nn.Module):
       self.w_o = nn.Linear(d_model, d_model, bias=False) # Wo
       self.dropout = nn.Dropout(dropout)
 
-  @staticmethod
-  def attention(query, key, value, mask, dropout: nn.Dropout):
-      d_k = query.shape[-1]
-      # Just apply the formula from the paper
-      # (batch, h, seq_len, d_k) --> (batch, h, seq_len, seq_len)
-      attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
-      if mask is not None:
-          # Write a very low value (indicating -inf) to the positions where mask == 0
-          min_val = torch.finfo(query.dtype).min 
-          attention_scores.masked_fill_(mask == 0, min_val) 
-      attention_scores = attention_scores.softmax(dim=-1) # (batch, h, seq_len, seq_len) # Apply softmax
-      if dropout is not None:
-          attention_scores = dropout(attention_scores)
-      # (batch, h, seq_len, seq_len) --> (batch, h, seq_len, d_k)
-      # return attention scores which can be used for visualization
-      return (attention_scores @ value), attention_scores
+  # @staticmethod
+  # def attention(query, key, value, mask, dropout: nn.Dropout):
+  #     d_k = query.shape[-1]
+  #     # Just apply the formula from the paper
+  #     # (batch, h, seq_len, d_k) --> (batch, h, seq_len, seq_len)
+  #     attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+  #     if mask is not None:
+  #         # Write a very low value (indicating -inf) to the positions where mask == 0
+  #         min_val = torch.finfo(query.dtype).min 
+  #         attention_scores.masked_fill_(mask == 0, min_val) 
+  #     attention_scores = attention_scores.softmax(dim=-1) # (batch, h, seq_len, seq_len) # Apply softmax
+  #     if dropout is not None:
+  #         attention_scores = dropout(attention_scores)
+  #     # (batch, h, seq_len, seq_len) --> (batch, h, seq_len, d_k)
+  #     # return attention scores which can be used for visualization
+  #     return (attention_scores @ value), attention_scores
 
-  def forward(self, q, k, v, mask):
-      query = self.w_q(q) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
-      key = self.w_k(k) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
-      value = self.w_v(v) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+  # def forward(self, q, k, v, mask):
+  #     query = self.w_q(q) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+  #     key = self.w_k(k) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+  #     value = self.w_v(v) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
 
-      # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
-      query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
-      key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
-      value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+  #     # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
+  #     query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+  #     key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+  #     value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
 
-      # Calculate attention
-      x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+  #     # Calculate attention
+  #     x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
       
-      # Combine all the heads together
-      # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
-      x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+  #     # Combine all the heads together
+  #     # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
+  #     x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
 
-      # Multiply by Wo
-      # (batch, seq_len, d_model) --> (batch, seq_len, d_model)  
-      return self.w_o(x)
+  #     # Multiply by Wo
+  #     # (batch, seq_len, d_model) --> (batch, seq_len, d_model)  
+  #     return self.w_o(x)
+  def forward(self, q, k, v, mask=None):
+    batch_size, seq_len, _ = q.shape
+    
+    query = self.w_q(q)
+    key = self.w_k(k)
+    value = self.w_v(v)
+
+    query = query.view(batch_size, seq_len, self.h, self.d_k).transpose(1, 2).contiguous()
+    key = key.view(batch_size, seq_len, self.h, self.d_k).transpose(1, 2).contiguous()
+    value = value.view(batch_size, seq_len, self.h, self.d_k).transpose(1, 2).contiguous()
+
+    causal = True 
+    softmax_scale = 1.0 / (self.d_k ** 0.5)
+    
+    attn_output = TritonAttention.apply(query, key, value, causal, softmax_scale) 
+    
+    # 合并多头: (batch, h, seq_len, d_k) -> (batch, seq_len, h, d_k) -> (batch, seq_len, d_model)    
+    attn_output = attn_output.transpose(1, 2).contiguous().view(batch_size, seq_len, self.d_model)
+    attn_output = self.dropout(attn_output)
+
+    return self.w_o(attn_output)
 
 class GPTBlock(nn.Module):
   def __init__(self, d_model, h, d_ff, dropout):
